657eccf52492076e904eadc104ef253ee664a5eb
Win Feigle <win.feigle@gmail.com>
2022-05-23 19:30:04 -0600

    commit tree 55a3c292c90126c7766d3bf4d318b0e6e53e05a4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+//no longer has DOM node 'main#main'
+document.querySelector('main#main').remove();
+
+//has a 'newHeader' variable that points to an <h1> node, 'h1#victory'
+const newHeader = document.createElement('h1');
+
+//the 'newHeader' variable points to a node that has an id of 'victory'
+newHeader.id = 'victory';
+
+//the 'newHeader' variable points to a node 'h1#victory', has "YOUR-NAME is the champion" inside
+newHeader.textContent = 'Win Feigle is the champion';
+
+//Append to document
+document.body.append(newHeader);
